1468a3c33d39360080d2bf47c1bf5b97cc21a32a
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-06-29 17:22:28 -0500

    [update files](<AirBnB_clone>)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
-from models.state import State
-from models.review import Review
-from models.place import Place
-from models.user import User
-from models.city import City
-from models.amenity import Amenity
+"""All the content of the FileStorage class""" 
+
 import json
-""" Module FileStorage """
+from models.base_model import BaseModel
+
 
 
 class FileStorage:
