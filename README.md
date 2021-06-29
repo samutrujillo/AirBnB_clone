d387fd97ee4ff074d26346a6f5c0a602915498c7
Sergioarg <sergio_ramos53@yahoo.com>
2021-06-29 14:07:07 -0500

    :tada: Create folder engine with files and readme files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# AirBnB_clone
-AirBnB_clone
+<img src="https://i.ibb.co/2NBYbYv/CLON1.png">
+---
+
+# Project: AirBnB clone
+
+_This project consists of making a clone of the original page <a href="https://www.airbnb.com/">AirBnB Clone</a>, in this folder we create a console Where we will manage and manipulate the JSON file where the information of the Basemodel object will be handled._
+
+# Console
+
+### ¿How use it?
+
+* Interactive mode
+```console
+$ ./console.py
+(hbnb) help
+
+Documented commands (type help <topic>):
+========================================
+EOF  help  quit
+
+(hbnb)
+(hbnb)
+(hbnb) quit
+$
+```
---
+++ engine/file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.user import User
+from models.city import City
+from models.amenity import Amenity
+import json
+""" Module FileStorage """
+
+
+class FileStorage:
+    """[File storage class]"""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''[return the object]'''
+        return self.__objects
+
+    def new(self, obj):
+        """[add new object]"""
+
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        """[Serializes __objects to the JSON file (path: __file_path)]"""
+        for key, value in self.__objects.items():
+            if not isinstance(value, dict):
+                self.__objects[key] = value.to_dict()
+        with open(self.__file_path, "w") as file:
+            json.dump(self.__objects, file)
+
+    def reload(self):
+        """[deserializes the JSON file to __objects]"""
+        try:
+            with open(self.__file_path, "r") as file:
+                serializable_file = json.load(file)
+            for key, value in serializable_file.items():
+                self.__objects[key] = eval(value['__class__'])(**value)
+        except FileNotFoundError:
+            pass
